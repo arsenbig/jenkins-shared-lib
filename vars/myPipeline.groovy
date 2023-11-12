591c96e3967b6ef7c766b6fb5ff378bb0df67347
arsenbig <arsenbig@gmail.com>
2023-11-13 03:31:17 +0400

    fx

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,39 +1,70 @@
-// mySharedLibrary/vars/myPipeline.groovy
+// vars/myPipeline.groovy
 
-class MyPipeline {
-    def call() {
-        return this
-    }
-
-    def checkoutCode() {
-        script {
-            checkout scm
-        }
-    }
-
-    def buildJavaApp() {
-        script {
-            sh 'mvn clean install'
-        }
-    }
-
-    def runTests() {
-        script {
-            sh 'mvn test'
-        }
-    }
-
-    def publishTestResults() {
-        script {
-            step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/*.xml'])
-        }
-    }
-
-    def buildDockerImage() {
-        script {
-            sh 'docker build -t my-java-app .'
-        }
+def checkoutCode() {
+    script {
+        checkout scm
     }
 }
 
-return new MyPipeline()
+def buildJavaApp() {
+    script {
+        sh 'mvn clean install'
+    }
+}
+
+def runTests() {
+    script {
+        sh 'mvn test'
+    }
+}
+
+def publishTestResults() {
+    script {
+        step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/*.xml'])
+    }
+}
+
+def buildDockerImage() {
+    script {
+        sh 'docker build -t my-java-app .'
+    }
+}
+
+def call() {
+    echo 'Running CI/CD pipeline...'
+
+    // Stage: Checkout
+    stage('Checkout') {
+        steps {
+            checkoutCode()
+        }
+    }
+
+    // Stage: Build
+    stage('Build') {
+        steps {
+            buildJavaApp()
+        }
+    }
+
+    // Stage: Test
+    stage('Test') {
+        steps {
+            runTests()
+        }
+    }
+
+    // Stage: Publish Test Results
+    stage('Publish Test Results') {
+        steps {
+            publishTestResults()
+        }
+    }
+
+    // Stage: Build Docker Image
+    stage('Build Docker Image') {
+        steps {
+            buildDockerImage()
+        }
+    }
+}
